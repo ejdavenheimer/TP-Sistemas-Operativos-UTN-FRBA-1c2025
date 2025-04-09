708805292ddb41e3728c0cf58ec51f8083ed96dc
AgustinCardozo <agus.cardozo96@gmail.com>
2025-04-09 17:48:21 -0300

    feat: soporte para log en utils

--- utils/config/config.go
+++ utils/config/config.go
@@ -6,6 +6,22 @@ import (
 	"os"
 )
 
+// InitConfig: lee el archivo de configuración y retorna sus valores en la variable config. En caso de error no se crea el archivo
+//
+// Parámetros:
+//   - filePath: ubicacion donde se encuentra el archivo de configuracion
+//   - config: acepta cualquier tipo de estructura
+//
+// Ejemplo:
+//
+//	type TestConfig struct {
+//		Name  string `json:"name"`
+//		Value int    `json:"value"`
+//	}
+//	func main() {
+//		var testConfig TestConfig
+//		config.InitConfig("./test.json", &testConfig)
+//	}
 func InitConfig(filePath string, config interface{}) {
 	err := setupConfig(filePath, &config)
 	if err != nil {
