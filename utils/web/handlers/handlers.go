0eb7a1ac5b6bf37b26cd3d8ef024a6edc3597e1e
AgustinCardozo <agus.cardozo96@gmail.com>
2025-04-11 22:53:13 -0300

    feat: soporte para cliente y servidor en utils

---
+++ utils/web/handlers/handlers.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"github.com/sisoputnfrba/tp-golang/utils/web/server"
+	"net/http"
+)
+
+// HandshakeHandler: se usa para chequear la conexión al servidor
+//
+// Parámetros:
+//   - message: el mensaje que querés devolver en la respuesta
+//
+// Ejemplo:
+//
+//	func main() {
+//		http.HandleFunc("/e", handlers.HandshakeHandler("Mensaje de ejemplo"))
+//
+//		err := server.InitServer(8001)
+//		if err != nil {
+//			slog.Error("init server error: ", err)
+//		}
+//	}
+func HandshakeHandler(message string) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		server.SendJsonResponse(writer, message)
+	}
+}
---
+++ utils/web/server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+// InitServer: inicializa el servidor, en caso de no poder levantarlo retorna un error
+//
+// Parámetros:
+//   - port: puerto donde se iniciará el servidor
+//
+// Ejemplo:
+//
+//	func main() {
+//		err := server.InitServer(globals.ConfigMemoria.Port)
+//		if err != nil {
+//			fmt.Errorf("error initializing server: %v", err)
+//			panic(err)
+//		}
+//	}
+func InitServer(port int) error {
+	addr := ":" + strconv.Itoa(port)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Println("Error al escuchar en el puerto " + addr)
+		fmt.Println(err)
+	}
+	return err
+}
+
+// SendJsonResponse: retorna la respues del servidor en formato JSON
+//
+// Parámetros:
+//   - writer: el http.ResponseWriter con el que se escribe la respuesta HTTP
+//   - data: cualquier estructura de datos que querés enviar al cliente, se convierte automáticamente a JSON.
+//
+// Ejemplo:
+//
+//	func HandshakeHandler(message string) func(http.ResponseWriter, *http.Request) {
+//		return func(writer http.ResponseWriter, request *http.Request) {
+//			server.SendJsonResponse(writer, message)
+//		}
+//	}
+func SendJsonResponse(writer http.ResponseWriter, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(writer, "Error al convertir datos a JSON", http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(response)
+}
