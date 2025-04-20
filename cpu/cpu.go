f1e4b5f69b6160311b06d2d32356ee28cae4771f
Belén Copa <ro2000copa@gmail.com>
2025-04-20 21:41:00 +0000

    Registra idCpu y genera archivo log

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	cpuHandler "github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/cpu/handlers"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/cpu/models"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/utils/config"
@@ -13,23 +14,36 @@ import (
 )
 
 const (
-	ConfigPath = "./configs/cpu.json"
-	LogPath    = "cpu.log"
+	ConfigPath = "cpu/configs/cpu.json"
+//	LogPath    = "cpu.log"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		slog.Error("Faltó el identificador de la CPU. Ejemplo: ./bin/cpu [identificador]")
+		return
+	}
+	idCpu := os.Args[1]
+
 	config.InitConfig(ConfigPath, &models.CpuConfig)
-	log.InitLogger(LogPath, models.CpuConfig.LogLevel)
+
+	logPath, err := log.BuildLogPath("cpu_%s", idCpu)
+	if err != nil {
+		slog.Error("No se pudo construir el log path", "err", err)
+		return
+	}
+
+	log.InitLogger(logPath, models.CpuConfig.LogLevel)
 
 	slog.Debug(fmt.Sprintf("Port cpu: %d", models.CpuConfig.PortCpu))
 
-	var cpuNumber int = 1 //TODO: revisar de donde sacamos el número de CPU => nombre de archivo de config?
+	//var cpuNumber int = 1 //TODO: revisar de donde sacamos el número de CPU => nombre de archivo de config?
 
-	http.HandleFunc("GET /", handlers.HandshakeHandler(fmt.Sprintf("Bienvenido al módulo de CPU%d", cpuNumber)))
+	http.HandleFunc("GET /", handlers.HandshakeHandler(fmt.Sprintf("Bienvenido al módulo de CPU%s", idCpu)))
 	http.HandleFunc("GET /cpu", handlers.HandshakeHandler("Cpu en funcionamiento 🚀"))
 	http.HandleFunc("POST /cpu/exec", cpuHandler.ExecuteHandler(models.CpuConfig))
 
-	err := server.InitServer(models.CpuConfig.PortCpu)
+	err = server.InitServer(models.CpuConfig.PortCpu)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error initializing server: %v", err))
 		panic(err)
