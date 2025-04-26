0139c1e146a468b5fcda812a4b80c447b86592ab
Belén Copa <ro2000copa@gmail.com>
2025-04-26 21:34:22 +0000

    Registra parametros

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"strconv"
 
 	kernelHandler "github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/kernel/handlers"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/kernel/models"
+	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/kernel/services"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/utils/config"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/utils/log"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/utils/web/handlers"
@@ -18,7 +21,23 @@ const (
 	LogPath    = "./logs/kernel.log"
 )
 
+var pcb *models.PCB
+
 func main() {
+	if len(os.Args) < 3 {
+		slog.Error("Faltan los parametros necesarios [archivo_pseudocódigo] y [tamanio_proceso]")
+		return
+	}
+
+	//Parametros
+	pseudocodeFile := os.Args[1]
+	processSize, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		slog.Error(fmt.Sprintf("Error al convertir el tamaño del proceso: %v", err))
+		return
+	}
+	additionalArgs := os.Args[3:]
+
 	//Test de funciones
 	//services.TestQueueNew()
 	//services.TestFinalizarProceso()
@@ -27,6 +46,12 @@ func main() {
 	log.InitLogger(LogPath, models.KernelConfig.LogLevel)
 
 	slog.Debug(fmt.Sprintf("Port Kernel: %d", models.KernelConfig.PortKernel))
+
+	//Iniciar proceso
+	pcb, err = services.InitProcess(pseudocodeFile, processSize, additionalArgs)
+	if err != nil{
+		slog.Error("Error al iniciar proceso", "err", err)
+	}
 
 	//TODO: borrar esto
 	//device := models2.Device{Ip: "127.0.0.1", Port: 8003, Name: "Test"}
@@ -43,7 +68,7 @@ func main() {
 	http.HandleFunc("POST /kernel/finalizarProceso", kernelHandler.FinishProcessHandler)
 
 	//Iniciacialización del servidor
-	err := server.InitServer(models.KernelConfig.PortKernel)
+	err = server.InitServer(models.KernelConfig.PortKernel)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error initializing server: %v", err))
 		panic(err)
