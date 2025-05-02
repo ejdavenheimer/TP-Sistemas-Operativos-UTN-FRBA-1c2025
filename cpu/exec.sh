0355715460c8df5976474e829148d29c90bef67d
AgustinCardozo <agus.cardozo96@gmail.com>
2025-05-02 19:59:47 +0000

    build: se actualizan archivos exec de cpu y kernel

--- cpu/exec.sh
+++ cpu/exec.sh
@@ -5,19 +5,19 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Validar que se pasaron al menos 2 parámetros: nombre y puerto
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
     echo -e "${RED}Error: Debes proporcionar un nombre de dispositivo y un puerto como parámetros.${NC}"
-    echo "Uso: ./exec.sh <nombre> [config_file]"
+    echo "Uso: ./exec.sh <nombre> <puerto> [config_file]"
     exit 1
 fi
 
 # Parámetros obligatorios
 IDENTIFICADOR="$1"
-# PORT="$2"
+PORT="$2"
 
 # Config por defecto u opcional
 DEFAULT_CONFIG="./configs/cpu.json"
-CONFIG_FILE="${2:-$DEFAULT_CONFIG}"
+CONFIG_FILE="${3:-$DEFAULT_CONFIG}"
 
 # Verificar si el archivo de configuración existe
 if [ ! -f "$CONFIG_FILE" ]; then
@@ -26,17 +26,17 @@ if [ ! -f "$CONFIG_FILE" ]; then
 fi
 
 # Compilar el archivo Go y capturar errores
-go build ./cpu.go 2> build_errors.log
+go build -o cpu ./cpu.go 2> build_errors.log
 
 # Verificar si la compilación fue exitosa
 if [ $? -eq 0 ]; then
     echo "Compilación exitosa."
     echo "Ejecutando con:"
     echo "Identificador: $IDENTIFICADOR"
-    # echo "Puerto: $PORT"
+    echo "Puerto: $PORT"
     echo "Config: $CONFIG_FILE"
 
-    ./cpu "$IDENTIFICADOR" "$CONFIG_FILE" 
+    ./cpu "$IDENTIFICADOR" "$PORT" "$CONFIG_FILE" 
 else
     echo -e "${RED}Error en la compilación. Revisa los detalles a continuación:${NC}"
     cat build_errors.log
--- kernel/exec.sh
+++ kernel/exec.sh
@@ -4,25 +4,39 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# Archivo de configuración por defecto
+# Validar que se pasaron al menos 2 parámetros: nombre y puerto
+if [ $# -lt 2 ]; then
+    echo -e "${RED}Error: Debes proporcionar un nombre de dispositivo y un puerto como parámetros.${NC}"
+    echo "Uso: ./exec.sh <nombre> <puerto> [config_file]"
+    exit 1
+fi
+
+# Parámetros obligatorios
+PATH="$1"
+SIZE="$2"
+
+# Config por defecto u opcional
 DEFAULT_CONFIG="./configs/kernel.json"
+CONFIG_FILE="${3:-$DEFAULT_CONFIG}"
 
-# Usar el archivo pasado por parámetro, o el default si no se especifica
-CONFIG_FILE="${1:-$DEFAULT_CONFIG}"
-
-# Verificar si el archivo existe
+# Verificar si el archivo de configuración existe
 if [ ! -f "$CONFIG_FILE" ]; then
     echo -e "${RED}Error: El archivo de configuración '$CONFIG_FILE' no existe.${NC}"
     exit 1
 fi
 
 # Compilar el archivo Go y capturar errores
-go build ./kernel.go 2> build_errors.log
+go build -o kernel ./kernel.go 2> build_errors.log
 
 # Verificar si la compilación fue exitosa
 if [ $? -eq 0 ]; then
-    echo "Compilación exitosa. Ejecutando el programa con config: $CONFIG_FILE"
-    ./kernel "$CONFIG_FILE"
+    echo "Compilación exitosa."
+    echo "Ejecutando con:"
+    echo "Path: $PATH"
+    echo "Tamaño de memoria: $SIZE"
+    echo "Config: $CONFIG_FILE"
+
+    ./kernel "$PATH" "$SIZE" "$CONFIG_FILE" 
 else
     echo -e "${RED}Error en la compilación. Revisa los detalles a continuación:${NC}"
     cat build_errors.log
