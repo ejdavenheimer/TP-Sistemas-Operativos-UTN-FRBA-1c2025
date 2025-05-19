11dee787b23edde3ba9b789c282f03fafedfcc55
mati-leon <tumail@tucasilla.com>
2025-05-19 09:37:48 -0300

    Logs obligatorios io

--- io/handlers/io_handlers.go
+++ io/handlers/io_handlers.go
@@ -7,6 +7,11 @@ import (
 	kernelModel "github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/kernel/models"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/utils/web/server"
 	"net/http"
+	"bufio"
+	"strings"
+	"log/slog"
+	"fmt"
+	"net"
 )
 
 func SleepHandler() func(http.ResponseWriter, *http.Request) {
@@ -36,5 +41,41 @@ func SleepHandler() func(http.ResponseWriter, *http.Request) {
 
 		server.SendJsonResponse(writer, response)
 		ioModel.DeviceMutex.Unlock()
+	}
+}
+
+//CONEXION CON EL KERNEL 
+func ConectToKernel(nombre, ip string, puerto int) { 
+	direccion := net.JoinHostPort(ip, fmt.Sprintf("%d", puerto))
+	conn, err := net.Dial("tcp", direccion) //Establece conexion TCP con el Kernel
+	if err != nil {
+		slog.Error("No se pudo conectar al Kernel", "error", err)
+		return
+	}
+	defer conn.Close()
+
+	// comunicacion inicial
+	fmt.Fprintf(conn, "%s\n", nombre)
+	slog.Info("Handshake enviado al Kernel", "nombre", nombre)
+
+	// Espera la petición
+	reader := bufio.NewReader(conn)
+	for {
+		linea, err := reader.ReadString('\n')
+		if err != nil {
+			slog.Error("Error leyendo petición del Kernel", "error", err)
+			break
+		}
+		linea= strings.TrimSpace(linea)
+		slog.Info("Petición recibida", "mensaje", strings.TrimSpace(linea))
+
+		// Analiza el tiempo de la peticion 
+		var pid, tiempo int
+		_, err = fmt.Sscanf(linea, "PID: %*d|TIEMPO_IO: %d", &pid, &tiempo)
+		if err != nil {
+			slog.Warn("Petición inválida", "detalle", linea)
+			continue
+		}
+		services.Sleep(pid, tiempo)
 	}
 }
