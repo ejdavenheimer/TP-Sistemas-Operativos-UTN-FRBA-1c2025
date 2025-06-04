fdc7266bda818d6b05cfe682f665626b6b4f0a96
AgustinCardozo <agus.cardozo96@gmail.com>
2025-06-04 18:35:52 -0300

    feat: obtener página de la cache

--- cpu/models/cache.go
+++ cpu/models/cache.go
@@ -1,31 +1,12 @@
 package models
 
-import (
-	"sync"
-)
-
 // CacheEntry representa una entrada en la caché de páginas.
 type CacheEntry struct {
 	PID        int // PID del proceso al que pertenece esta página (esencial para desalojo)
 	PageNumber int // Identificador único de la página lógica
 	//PhysicalAddress int       // Dirección física en memoria (para escribir de vuelta a MP)
 	Content     []byte // Contenido de la página (bytes)
-	UsageBit    bool   // Bit de Uso (U): true si la página fue accedida recientemente
+	UseBit      bool   // Bit de Uso (U): true si la página fue accedida recientemente
 	ModifiedBit bool   // Bit de Modificación (M): true si la página fue escrita en caché
 	//TiempoCarga time.Time // Momento en que la página fue cargada en caché
-}
-
-// PageCache representa la caché de páginas de la CPU.
-type PageCache struct {
-	Entries      []CacheEntry // Slice de punteros a entradas de caché para permitir nil
-	MaxEntries   int          // Cantidad máxima de entradas
-	Algorithm    string       // Algoritmo de reemplazo (CLOCK o CLOCK-M)
-	ClockPointer int          // Puntero para el algoritmo CLOCK/CLOCK-M
-	Mutex        sync.Mutex   // Mutex para control de concurrencia
-	//PageSize     int          // Tamaño de cada página (para leer de memoria)
-	// Mapa para búsqueda rápida: {PID + PageNumber} -> Índice en Entries
-	PageMap map[struct {
-		PID        int
-		PageNumber int
-	}]int
 }
--- cpu/services/cache_service.go
+++ cpu/services/cache_service.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/cpu/models"
 	"log/slog"
+	"sync"
 )
 
-func InitCache() *models.PageCache {
+// PageCache representa la caché de páginas de la CPU.
+type PageCache struct {
+	Entries      []models.CacheEntry // Slice de punteros a entradas de caché para permitir nil
+	MaxEntries   int                 // Cantidad máxima de entradas
+	Algorithm    string              // Algoritmo de reemplazo (CLOCK o CLOCK-M)
+	ClockPointer int                 // Puntero para el algoritmo CLOCK/CLOCK-M
+	Mutex        sync.Mutex          // Mutex para control de concurrencia
+	//PageSize     int          // Tamaño de cada página (para leer de memoria)
+	// Mapa para búsqueda rápida: {PID + PageNumber} -> Índice en Entries
+	PageMap map[struct {
+		PID        int
+		PageNumber int
+	}]int
+}
+
+func InitCache() *PageCache {
 	maxEntries := models.CpuConfig.CacheEntries
 
 	//Debería ser un valor positivo pero si llega a venir negativo lo pongo en 0
@@ -14,7 +30,7 @@ func InitCache() *models.PageCache {
 		maxEntries = 0
 	}
 
-	cache := &models.PageCache{
+	cache := &PageCache{
 		Entries:      make([]models.CacheEntry, 0, maxEntries),
 		MaxEntries:   maxEntries,
 		Algorithm:    models.CpuConfig.CacheReplacement,
@@ -43,4 +59,29 @@ func getEntryKey(pid, pageNumber int) struct {
 		PID        int
 		PageNumber int
 	}{PID: pid, PageNumber: pageNumber}
+}
+
+// Get intenta obtener una página de la caché.
+// Retorna el contenido de la página y true si es un caché hit.
+func (cache *PageCache) Get(pid, page int) ([]byte, bool) {
+	cache.Mutex.Lock()
+	defer cache.Mutex.Unlock()
+
+	if !IsEnabled(cache.MaxEntries) {
+		return nil, false
+	}
+
+	key := getEntryKey(pid, page)
+	index, found := cache.PageMap[key]
+	if !found {
+		//TODO: revisar que pasa en este caso, entiendo que tendría que revisar si se encuentra en la TLB.
+		//Cache MISS
+		slog.Debug(fmt.Sprintf("Cache MISS: PID %d, Page %d", pid, page))
+		return nil, false
+	}
+
+	//Cache HIT
+	cache.Entries[index].UseBit = true
+	slog.Debug(fmt.Sprintf("Cache HIT: PID %d, Page %d (slot %d). Content: %s", pid, page, index, cache.Entries[index].Content))
+	return cache.Entries[index].Content, true
 }
