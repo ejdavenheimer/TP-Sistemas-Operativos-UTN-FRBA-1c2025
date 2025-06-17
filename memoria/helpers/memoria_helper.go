7d04d038d296b502b1ea58182505ee68bd9d41b4
AgustinCardozo <agus.cardozo96@gmail.com>
2025-06-17 03:09:19 -0300

    feat: soporte inicial para escribir archivo dump

--- memoria/helpers/memoria_helper.go
+++ memoria/helpers/memoria_helper.go
@@ -24,12 +24,12 @@ func CreateDirectory(dir string) {
 }
 
 // CreateFile crea el archivo
-func CreateFile(fileName string, size int) error {
+func CreateFile(fileName string, size int) (*os.File, error) {
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0666)
 
 	if err != nil {
 		slog.Error(fmt.Sprintf("Error al crear el archivo: %v", err))
-		return err
+		return nil, err
 	}
 
 	defer file.Close()
@@ -38,10 +38,10 @@ func CreateFile(fileName string, size int) error {
 	err = file.Truncate(int64(size))
 	if err != nil {
 		slog.Error(fmt.Sprintf("Error al ajustar el tamaño del archivo: %v", err))
-		return err
+		return nil, err
 	}
 
-	return nil
+	return file, nil
 }
 
 // InitMemory se encarga de hacer la configuración inicial
@@ -64,7 +64,7 @@ func InitMemory(configPath string, logPath string) {
 
 	CreateDirectory(models.MemoryConfig.DumpPath)
 	slog.Debug(fmt.Sprintf("Swap: %s", models.MemoryConfig.SwapFilePath))
-	_ = CreateFile(models.MemoryConfig.SwapFilePath, 0) //TODO: revisar, inicialmente va arrancar con tamaño 0
+	_, _ = CreateFile(models.MemoryConfig.SwapFilePath, 0) //TODO: revisar, inicialmente va arrancar con tamaño 0
 }
 
 func GetDumpName(pid uint) string {
--- memoria/services/dump_service.go
+++ memoria/services/dump_service.go
@@ -7,16 +7,48 @@ import (
 	"log/slog"
 )
 
-func ExecuteDumpMemory(pid uint, size int, path string) error{
+func ExecuteDumpMemory(pid uint, size int, path string) error {
 	slog.Info(fmt.Sprintf("## PID: %d - Memory Dump solicitado", pid)) //Log obligatorio
-	dumpName := helpers.GetDumpName(pid) //obtiene el nombre del archivo 
-	
+	dumpName := helpers.GetDumpName(pid)                               //obtiene el nombre del archivo
+
 	//crea o abre el archivo
-	err := helpers.CreateFile(models.MemoryConfig.DumpPath + dumpName, size)
+	dumpFilePath := models.MemoryConfig.DumpPath + dumpName
+	file, err := helpers.CreateFile(dumpFilePath, size)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error: %v", err))
 		return err
 	}
-	
+
+	dumpContent := make([]byte, size)
+	numberPages := size / models.MemoryConfig.PageSize
+
+	if size%models.MemoryConfig.PageSize != 0 {
+		numberPages++ // Si el tamaño no es múltiplo de PageSize, redondear hacia arriba
+	}
+
+	for pageNum := 0; pageNum < numberPages; pageNum++ {
+		pageContent, err := GetPageContent(pid, pageNum) // Asumo este método en MemoriaService
+		if err != nil {
+			slog.Error(fmt.Sprintf("Memoria: Fallo al leer página %d para PID %d durante el dump", pageNum, pid))
+			// Decidir si continuar o abortar el dump. Por simplicidad, abortamos aquí.
+			return fmt.Errorf("fallo al leer página %d durante el dump: %w", pageNum, err)
+		}
+		dumpContent = append(dumpContent, pageContent...)
+	}
+
+	// 5. Escribir el contenido en el archivo
+	_, err = file.Write(dumpContent[:size]) // Escribir solo hasta el 'size' real
+	if err != nil {
+		slog.Error(fmt.Sprintf("Memoria: Fallo al escribir contenido en el archivo de dump '%s'", dumpFilePath))
+		return fmt.Errorf("fallo al escribir datos al archivo de dump: %w", err)
+	}
+
+	slog.Info(fmt.Sprintf("Memoria: Memory Dump completado para PID %d. Archivo: %s", pid, dumpFilePath))
+
 	return nil
+}
+
+func GetPageContent(pid uint, pageNum int) ([]byte, error) {
+	slog.Warn("TODO: implementar ")
+	return nil, nil
 }
