8f9451c51ff9c5a058946c79b602117878961b7a
AgustinCardozo <agus.cardozo96@gmail.com>
2025-07-11 11:25:43 -0300

    refactor: se crea servicio para syscall

--- kernel/services/short_term_scheduler.go
+++ kernel/services/short_term_scheduler.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	ioModel "github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/io/models"
-	"github.com/sisoputnfrba/tp-2025-1c-Los-magiOS/kernel/helpers"
 	"log/slog"
 	"net/http"
 	"strconv"
@@ -112,80 +110,13 @@ func runProcessInCPU(pcb *models.PCB, cpu cpuModels.CpuN, CPUID string) {
 	case models.NeedExecuteSyscall:
 		syscallName := result.SyscallRequest.Type
 		slog.Info(fmt.Sprintf("## %d - Solicitó syscall: %s", result.SyscallRequest.Pid, syscallName))
+
 		if syscallName == "IO" {
-			// Primero chequea si existe el dispositivo
-			deviceRequested, index, exists := models.ConnectedDeviceList.Find(func(d ioModel.Device) bool {
-				return result.SyscallRequest.Values[0] == d.Name
-			})
-
-			if !exists || deviceRequested.Name == "" {
-				slog.Error(fmt.Sprintf("No se encontro al dispositivo %s", result.SyscallRequest.Values[0]))
-				EndProcess(result.SyscallRequest.Pid, fmt.Sprintf("No se encontro al dispositivo %s", result.SyscallRequest.Values[0]))
-				//server.SendJsonResponse(writer, map[string]interface{}{
-				//	"action": "exit",
-				//})
-				return
-			}
-
-			// Se crea una lista auxiliar con dispositivos del mismo tipo
-			connectedDeviceListAux := models.ConnectedDeviceList.FindAll(func(d ioModel.Device) bool {
-				return result.SyscallRequest.Values[0] == d.Name
-			})
-
-			// Se busca los dispositivos que se encuentren libres
-			deviceRequestedAux, indexAux, exists := connectedDeviceListAux.Find(func(d ioModel.Device) bool {
-				return d.IsFree
-			})
-
-			// En caso de que no encuentre ningún dispositivo libre, se bloquea el proceso
-			if index < 0 || !exists {
-				slog.Debug("El dispositivo se encuentra ocupado...")
-				helpers.AddPidForDevice(deviceRequested.Name, int(result.SyscallRequest.Pid))
-				BlockedProcess(result.SyscallRequest.Pid, fmt.Sprintf("El dispositivo %s no se encuentra disponible", result.SyscallRequest.Type))
-				//server.SendJsonResponse(writer, map[string]interface{}{
-				//	"action": "block",
-				//})
-				return
-			}
-
-			deviceRequestedAux.IsFree = false
-			deviceRequestedAux.PID = result.SyscallRequest.Pid
-			err := models.ConnectedDeviceList.Set(index+indexAux, deviceRequestedAux)
-			if err != nil {
-				slog.Error(fmt.Sprintf("error: %v", err))
-				return
-			}
-
-			device := ioModel.Device{Ip: deviceRequestedAux.Ip, Port: deviceRequestedAux.Port, Name: result.SyscallRequest.Values[0]}
-			sleepTime, _ := strconv.Atoi(result.SyscallRequest.Values[1])
-			err = SleepDevice(result.SyscallRequest.Pid, sleepTime, device)
-
-			if err != nil {
-				slog.Error(fmt.Sprintf("error: %v", err))
-				return
-			}
-
-			//Acá se bloquea el proceso
-			//server.SendJsonResponse(writer, map[string]interface{}{
-			//	"action": "block",
-			//})
+			ExecuteIO(result)
 		}
 
 		if syscallName == "DUMP_MEMORY" {
-			pcb, index, exists := models.QueueExec.Find(func(pcb *models.PCB) bool {
-				return pcb.PID == result.SyscallRequest.Pid
-			})
-			if !exists || index == -1 {
-				slog.Warn("TODO: ver que pasa en este caso por ahora hago un exit")
-				//server.SendJsonResponse(writer, map[string]string{
-				//	"action": "exit",
-				//})
-				return
-			}
-			DumpServices(uint(pcb.PID), pcb.Size)
-			//server.SendJsonResponse(writer, map[string]string{
-			//	"action": "continue",
-			//})
+			ExecuteDUMP(result)
 		}
 	}
 	// Liberar CPU
